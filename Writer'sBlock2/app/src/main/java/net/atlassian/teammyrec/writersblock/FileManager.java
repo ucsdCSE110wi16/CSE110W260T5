cc5fe68664875e2e7278c28844b42e32aada3061
Matt <mattporter2718@gmail.com>
2016-02-06 10:02:10 -0800

    Begin working on file handling class

---
+++ Writer'sBlock2/app/src/main/java/net/atlassian/teammyrec/writersblock/FileManager.java
@@ -0,0 +1,25 @@
+package net.atlassian.teammyrec.writersblock;
+import java.io.File;
+/**
+ * Created by matt on 2/1/16.
+ */
+public class FileManager {
+
+    public File temp;
+    public File perm;
+
+    public FileManager(File temp, File perm) {
+        this.temp = temp;
+        this.perm = perm;
+
+    }
+
+    public void pushTemporaryFileToPermanent() {
+
+    }
+
+    public void updateTemp() {
+
+
+    }
+}
