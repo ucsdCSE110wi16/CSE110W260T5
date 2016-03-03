092855936c427152b9d383e9378d2ccd75099090
dheerajgottipalli <dheeraj.gottipalli@yahoo.com>
2016-03-02 16:55:32 -0800

    LINKING ALL DONE

--- app/src/main/java/net/atlassian/teammyrec/writersbloc/PhraseLinker.java
+++ app/src/main/java/net/atlassian/teammyrec/writersbloc/PhraseLinker.java
@@ -3,90 +3,88 @@ import java.util.*;
 import android.util.*;
 import net.atlassian.teammyrec.writersbloc.Models.DataModels.Page;
 
-public class PhraseLinker { 
+public class PhraseLinker {
 
-	/* 
-	 * findPhrases()
-	 * Description: Finds all of the keyphrases within a body of text. 
-	 */ 
-	public static PriorityQueue< Pair<Integer, Page> > findPhrases(String body, PriorityQueue<Page> keyPhrases) {
+    /*
+     * findPhrases()
+     * Description: Finds all of the keyphrases within a body of text.
+     */
+    public static PriorityQueue< Pair<Integer, Page> > findPhrases(String body, PriorityQueue<Page> keyPhrases) {
         PairComparator pc = new PairComparator();
-	    PriorityQueue< Pair<Integer, Page> > links = new PriorityQueue< Pair<Integer, Page> >(10, pc);
+        PriorityQueue< Pair<Integer, Page> > links = new PriorityQueue< Pair<Integer, Page> >(10, pc);
         if(body == null){
             return links;
         }
-		boolean[] hasBeenLinked = new boolean[body.length()];
+        boolean[] hasBeenLinked = new boolean[body.length()];
 
-		// Valid delimiters that can be to the left or right of the keyphrase
-		ArrayList<String> delimiters = new ArrayList<String>(
-			Arrays.asList(new String[] {" ", "\n", "'", "\"", ":", "-", ".", "!", ";", "?", ")", "("}));
-		
-		
-		// Check each keyphrase to see if it's part of the body
-		for(Page page : keyPhrases) {
-			String phrase = page.toString();
-			boolean addToLinks = true; 
-			int localIndex = body.toLowerCase().indexOf(phrase.toLowerCase());
-			int globalIndex = localIndex; 
-			if(localIndex != -1) {
-				String newBody = body; 
-				
-				do {
+        // Valid delimiters that can be to the left or right of the keyphrase
+        ArrayList<String> delimiters = new ArrayList<String>(
+                Arrays.asList(new String[] {" ", "\n", "'", "\"", ":", "-", ".", "!", ";", "?", ")", "("}));
+
+
+        // Check each keyphrase to see if it's part of the body
+        for(Page page : keyPhrases) {
+            String phrase = page.toString();
+            boolean addToLinks = true;
+            int localIndex = body.toLowerCase().indexOf(phrase.toLowerCase());
+            int globalIndex = localIndex;
+            if(localIndex != -1) {
+                String newBody = body;
+
+                do {
                     addToLinks = true;
                     System.out.println("BEG OF LOOP: globalIndex = " + globalIndex);
                     System.out.println(phrase + " has been found at index " + globalIndex);
-					String beforeDelim;
-					String afterDelim;
+                    String beforeDelim ;
+                    String afterDelim;
 
-					// If the phrase is at the beginning of the string, just set delim to whitespace 
-					if(globalIndex - 1 >= 0) { 
-						beforeDelim = new String(new char[] {body.charAt(globalIndex- 1)});
-					} else { 
-						beforeDelim = " ";
-					}
+                    // If the phrase is at the beginning of the string, just set delim to whitespace
+                    if(globalIndex - 1 >= 0) {
+                        beforeDelim = new String(new char[] {body.charAt(globalIndex- 1)});
+                    } else {
+                        beforeDelim = " ";
+                    }
 
-					// Same with end of string 
-					if(globalIndex + phrase.length() < body.length()) { 
-						afterDelim = new String(new char[] {body.charAt(globalIndex+phrase.length() )});
-					} else { 
-						afterDelim = " ";
-					}
+                    // Same with end of string
+                    if(globalIndex + phrase.length() < body.length()) {
+                        afterDelim = new String(new char[] {body.charAt(globalIndex+phrase.length() )});
+                    } else {
+                        afterDelim = " ";
+                    }
 
-					// If any part of the phrase is part of another link, don't add 
-					for(int i = globalIndex; i < globalIndex + phrase.length(); i++) { 
-						if(hasBeenLinked[i]) {
+                    // If any part of the phrase is part of another link, don't add
+                    for(int i = globalIndex; i < globalIndex + phrase.length(); i++) {
+                        if(hasBeenLinked[i]) {
                             System.out.println("Index " + i + " has already been linked.");
-							addToLinks = false; 
-							//break;
-							continue;
-						}
-					}
+                            //addToLinks = false;
+                            //break;
+                        }
+                    }
 
                     if(!addToLinks) System.out.println("We're not adding " + phrase + " to links this time.");
 
-					// Check that they're valid delimiters - this indicates the token is an intended link 
-					if(delimiters.contains(beforeDelim) && delimiters.contains(afterDelim)) { 
-						if(addToLinks) {
+                    // Check that they're valid delimiters - this indicates the token is an intended link
+                    if(delimiters.contains(beforeDelim) && delimiters.contains(afterDelim)) {
+                        if(addToLinks) {
                             links.add(new Pair(new Integer(globalIndex), page));
 
-							// Mark entire phrase in body as part of a link
-							for(int i = globalIndex; i < globalIndex + phrase.length(); i++) { 
-								hasBeenLinked[i] = true; 
-							}
-						}
-					}
-					globalIndex = localIndex + phrase.length();
-                    System.out.println("END OF LOOP: localIndex = " + localIndex);
-					newBody = newBody.substring(localIndex + phrase.length());
-                    System.out.println("newBody: " + newBody);
-					localIndex = newBody.toLowerCase().indexOf(phrase.toLowerCase());
-					globalIndex += localIndex;
-                    System.out.println("END OF LOOP: globalIndex = " + globalIndex);
-				} while(localIndex != -1);
-			}
-		}
-		return links;
-	}
+                            // Mark entire phrase in body as part of a link
+                            for(int i = globalIndex; i < globalIndex + phrase.length(); i++) {
+                                //hasBeenLinked[i] = true;
+                            }
+                        }
+                    }
+
+                    globalIndex += phrase.length();
+                    newBody = newBody.substring(localIndex + phrase.length());
+                    localIndex = newBody.toLowerCase().indexOf(phrase.toLowerCase());
+                    globalIndex += localIndex;
+                    System.out.println("Local index is : " + localIndex);
+                } while(localIndex != -1);
+            }
+        }
+        return links;
+    }
 
 }
 
@@ -97,8 +95,8 @@ class StringLengthComparator implements Comparator<String>
     @Override
     public int compare(String x, String y)
     {
-    	if(x.length() == y.length()) return 0;
-     	return (x.length() > y.length()? -1 : 1);
+        if(x.length() == y.length()) return 0;
+        return (x.length() > y.length()? -1 : 1);
     }
 }
 
@@ -117,4 +115,5 @@ class PairComparator implements Comparator< Pair<Integer, Page> >
         }
     }
 }
+
 
