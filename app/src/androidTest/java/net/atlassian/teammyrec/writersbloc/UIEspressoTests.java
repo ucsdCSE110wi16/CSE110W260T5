be452ca852d4cc2562e883d72701ee0714878bc7
willtchiu <wtchiu@ucsd.edu>
2016-03-05 22:44:02 -0800

    Added UITest

--- app/src/androidTest/java/net/atlassian/teammyrec/writersbloc/UIEspressoTests.java
+++ app/src/androidTest/java/net/atlassian/teammyrec/writersbloc/UIEspressoTests.java
@@ -1,5 +1,12 @@
 package net.atlassian.teammyrec.writersbloc;
 
+
+import android.support.test.uiautomator.By;
+import android.support.test.uiautomator.UiDevice;
+import android.support.test.uiautomator.UiObject2;
+import android.support.test.uiautomator.Until;
+import android.test.InstrumentationTestCase;
+
 import org.junit.Test;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -11,8 +18,46 @@ import static android.support.test.espresso.matcher.ViewMatchers.withId;
 /**
  * Created by williamchiu on 3/1/16.
  */
-public class UIEspressoTests {
+public class UIEspressoTests extends InstrumentationTestCase {
 
+    private UiDevice device;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        device = UiDevice.getInstance(getInstrumentation());
+        device.pressHome();
+        device.wait(Until.hasObject(By.text("Apps")), 3000);
+        UiObject2 appsButton = device.findObject(By.text("Apps"));
+        appsButton.click();
+        device.wait(Until.hasObject(By.text("WritersBloc")), 3000);
+        UiObject2 writersBlocApp = device.findObject(By.text("WritersBloc"));
+        writersBlocApp.click();
+        //device.waitForIdle(3000);
+    }
+
+    public void testSignInButton() throws Exception {
+
+        device.wait(Until.hasObject(By.text("Login:")), 3000);
+        device.wait(Until.hasObject(By.clazz("EditText")), 3000);
+        UiObject2 userText = device.findObject(By.text("User Name:"));
+        userText.setText("demouser");
+
+        UiObject2 passText = device.findObject(By.res("net.atlassian.teammyrec.writersbloc", "password_edit_activity_login"));
+        passText.setText("demouser");
+
+        device.wait(Until.hasObject(By.text("Submit")), 3000);
+        UiObject2 buttonSubmit = device.findObject(By.text("Submit"));
+        buttonSubmit.click();
+
+        device.waitForIdle(3000);
+
+        UiObject2 listView = device.findObject(By.clazz("ListView"));
+
+        assertTrue(listView != null);
+    }
+
+    /*
     @Test
     public void signUpButtonPopsViewOver() {
         onView(withId(R.id.button2)).perform(click()).check(matches(isDisplayed()));
@@ -37,4 +82,5 @@ public class UIEspressoTests {
     public void onTapCreateNewPage() {
         onView(withId(R.id.overlayAddPage)).perform(click()).check(matches(isDisplayed()));
     }
+    */
 }
